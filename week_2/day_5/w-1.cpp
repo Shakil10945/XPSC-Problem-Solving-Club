fbe66008d2e655822b4d796120a683a1c7cd1e69
shakil10945 <shakil10945@gmail.com>
2024-11-07 11:32:27 +0600

    week-2

--- week_2/day_5/w-1.cpp
+++ week_2/day_5/w-1.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
-    
+    int n, x;
+    cin>>n>>x;
+
+    vector<int> v;
+    for(int i=0; i<n; i++)
+    {
+        int m;
+        cin>>m;
+
+        if(m!=x)        v.push_back(m);
+    }
+
+    for(auto &l : v)
+        cout<< l<<" ";
+    cout<<endl;
+
 
     return 0;
 }
