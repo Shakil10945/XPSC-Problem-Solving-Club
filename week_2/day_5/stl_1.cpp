9f6a5dcfbfb935396e3b490f93eb8a5c0bd4a48d
shakil10945 <shakil10945@gmail.com>
2024-11-07 19:40:01 +0600

    stl-1

---
+++ week_2/day_5/stl_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n,m;    cin>>n>>m;
+    vector<int> v(n);
+    for(auto &x: v) cin>>x;
+
+    set<int> myset;
+
+    
+
+    while (m--)
+    {
+        int x; cin>>x;
+
+        
+    }
+    
+    
+
+    return 0;
+}
