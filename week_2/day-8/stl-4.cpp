608561f467e7678507f14088c56a8439160393ac
shakil10945 <shakil10945@gmail.com>
2024-11-13 20:21:58 +0600

    done

--- week_2/day-8/stl-4.cpp
+++ week_2/day-8/stl-4.cpp
@@ -12,26 +12,33 @@ int main()
         cin.ignore();
         int n, k;
         cin >> n >> k;
+        map<int, set<int>> my_map;
 
-        vector<int> v;
-        while (n--)
+        for(int i=1; i<=n; i++)
         {
             int x;
-            cin >> x;
-            v.push_back(x);
+            cin>>x;
+
+            my_map[x].insert(i);
         }
         while (k--)
         {
             int l, r;
             cin >> l >> r;
-            auto it = find(v.begin(), v.end(), l);
-            auto itt = find(it, v.end(), r);
-            if (itt != v.end())
+
+            if(my_map.find(l) == my_map.end() || my_map.find(r) == my_map.end())
             {
-                cout << "YES" << endl;
-                continue;
+                cout<<"NO"<<endl;
             }
-            cout << "NO" << endl;
+            else
+            {
+                int left = *my_map[l].begin();
+                int right = *my_map[r].rbegin();
+
+                if(left<right)      cout<<"YES"<<endl;
+                else                cout<<"NO"<<endl;
+            }
+
         }
     }
     return 0;
