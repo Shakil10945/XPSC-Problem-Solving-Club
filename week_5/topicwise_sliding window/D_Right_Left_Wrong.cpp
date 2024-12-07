0faffbe4c7e312247363ce92608436b39fba449d
shakil10945 <shakil10945@gmail.com>
2024-12-07 19:29:01 +0600

    done

--- week_5/topicwise_sliding window/D_Right_Left_Wrong.cpp
+++ week_5/topicwise_sliding window/D_Right_Left_Wrong.cpp
@@ -1,43 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
+int n;
+vector<int> vec;
+vector<long long> ps;
+string s;
+
+void input() 
 {
-    int n;  cin>>n;
-    vector<int> vec(n);
-    vector<int> ps(n+1, 0);
-    string s;
+    cin >> n;
+    vec.resize(n);
+    ps.resize(n + 1, 0);
 
-    for(int i=1; i<=n; i++)
+    for (int i = 1; i <= n; i++) 
     {
-        cin>>vec[i-1];
-        ps[i] = vec[i-1] + ps[i-1];
+        cin >> vec[i - 1];
+        ps[i] = ps[i - 1] + vec[i - 1];
     }
-    cin>>s;
-
-    int i=0, j=n-1, total = 0;
-    while (i<j)
-    {
-        while(i<n && s[i] != 'L')   i++;
-        if(i==n)        break;
-
-        while(j>=0 && s[j] != 'R')      j--;
-        if(j == -1 && j<i)      break;
-
-        total += ps[j+1]-ps[i];
-        i++;
-        j--;
-    }
-    cout<<total<<endl;
+    cin >> s;
 }
 
-int main()
+void solve() 
+{
+    input();
+
+    int i = 0, j = n - 1;
+    long long total = 0;
+    while (i < j) 
+    {
+        while (i < n && s[i] != 'L')    i++;
+        while (j >= 0 && s[j] != 'R')   j--;
+
+        if (i <= j) 
+        {
+            total += ps[j + 1] - ps[i];
+            i++;
+            j--;
+        } 
+        else    break;
+    }
+    cout << total << endl;
+}
+
+int main() 
 {
     ios::sync_with_stdio(false);    cin.tie(nullptr);
-    int t;  cin>>t;
-    while (t--)
-    {
-        solve();
-    }
+
+    int t;
+    cin >> t;
+    while (t--) solve();
     return 0;
 }
