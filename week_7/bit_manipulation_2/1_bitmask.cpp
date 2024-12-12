82ad0aa1a4d55c4aaf31c50f2dd56ccf7d3fb70b
Shakil Khan <shakil10945@gmail.com>
2024-12-12 14:45:00 +0600

    done

---
+++ week_7/bit_manipulation_2/1_bitmask.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    // n=5;
+
+    for(int mask = 0; mask< (1<<n); mask++)
+    {
+        cout<< mask<< " -> ";
+        for(int k=0; k<n; k++)
+        {
+            if((mask >> k) &1)
+                cout<<1<<" ";
+            else
+                cout<<0<< " ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
