fe39015cd4486408172146ae039e701da65493b7
shakil10945 <shakil10945@gmail.com>
2024-12-19 01:55:25 +0600

    doen

--- week_8/New folder/Stable_Array.cpp
+++ week_8/New folder/Stable_Array.cpp
@@ -14,25 +14,30 @@ int main()
         vector<int>ar(n);
         for(int i=0; i<n; i++)  cin>>ar[i];
 
-        set<int> my_set;        map<int, int> my_map;
+        multiset<int> my_set;
         for(int i=0; i<n; i++)
-        {
             my_set.insert(ar[i]);
-            my_map[ar[i]]++;
-        }
         
-        int max_num = *my_set.rbegin();
+        int max_num = *(--(my_set.end()));
 
         int r=0, l=0;
         int count = 0;
 
-        while (r<n-1)
+        while (r<n)
         {
             if(ar[r] == max_num)
             {
                 count = max(count, r-l);
-                max_num = max_elements(ar.begin()+r, ar.end());
+                l= r+1;
             }
+            auto it = my_set.find(ar[r]);
+            my_set.erase(it);
+            if(!my_set.count(max_num))
+            {
+                if(!my_set.empty())
+                    max_num = *(prev(my_set.end()));
+            }
+            r++;
         }
 
         cout<<count<<endl;
