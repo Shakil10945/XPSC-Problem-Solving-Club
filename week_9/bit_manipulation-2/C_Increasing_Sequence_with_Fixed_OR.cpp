7dd5164370a3e9f92c87c6f3c0606749e4976710
shakil10945 <shakil10945@gmail.com>
2024-12-23 17:56:46 +0600

    done

--- week_9/bit_manipulation-2/C_Increasing_Sequence_with_Fixed_OR.cpp
+++ week_9/bit_manipulation-2/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-
+long long n;
 void input()
 {
     cin>>n;
@@ -12,9 +11,25 @@ void solve()
 {
     input();
 
-    
+    vector<long long int> vec;
 
-    
+    for(int k=0; k<=__lg(n); k++)
+    {
+        if((n>>k)&1)
+        {
+            long long value = n - (1LL << k);
+            if(value>0)
+                vec.push_back(value);
+        }
+    }
+    vec.push_back(n);
+    sort(vec.begin(), vec.end());
+
+    cout<<vec.size()<<endl;
+
+    for(auto x: vec)
+        cout<<x<<" ";
+    cout<< endl;    
 }
 
 int main()
@@ -25,3 +40,38 @@ int main()
 
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+
+//    int t;
+//    cin >> t;
+//    while (t--) {
+//       long long n;
+//       cin >> n;
+//       deque<long long> ans;
+//       for (int k = 0;k <= __lg(n);k++) {
+//          if ((n >> k) & 1) {
+//             long long value = n - (1LL << k);
+//             if (value > 0) {
+//                ans.push_front(value);
+//                cout<<value<<" ";
+//             }
+//          }
+//       }
+
+//       ans.push_back(n);
+//     //cout << ans.size() << '\n';
+//     //   for (auto value : ans) {
+//     //      cout << value << " ";
+//     //   }
+//       cout << '\n';
+//    }
+
+//    return 0;
+// }
