f167ee60b961c418fd59ce44bc56596957142037
Shakil Khan <shakil10945@gmail.com>
2025-02-16 13:05:18 +0600

    done

--- One_last_try/week_2_STL-2/B_Sereja_and_Suffixes.cpp
+++ One_last_try/week_2_STL-2/B_Sereja_and_Suffixes.cpp
@@ -3,20 +3,44 @@ using namespace std;
 
 int n, m;
 vector<int> vec;
+vector<int> vec2;
 
 void input()
 {
     cin >> n >> m;
     vec.resize(n);
+    vec2.resize(m);
     for (int i = 0; i < n; i++)
         cin >> vec[i];
+    for (int i = 0; i < m; i++)
+        cin >> vec2[i];
 }
 
 void solve()
 {
     input();
     
+    map<int, int> my_map;
+    for(int i=0;i<n;i++)
+    {
+        my_map[vec[i]]++;
+    }
 
+    vector<int>prefix_size;
+    for(int i=0;i<n; i++)
+    {
+        prefix_size.push_back(my_map.size());
+
+        my_map[vec[i]]--;
+        if(my_map[vec[i]] == 0)
+        {
+            my_map.erase(vec[i]);
+        }
+    }
+    for(int i=0; i<m; i++)
+    {
+        cout<<prefix_size[vec2[i]-1]<<endl;
+    }
 }
 
 int main()
