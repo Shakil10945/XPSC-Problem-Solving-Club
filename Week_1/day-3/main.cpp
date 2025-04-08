ca11e52415d95d848b4fa2f79dded080b0519ebe
Shakil Khan <shakil10945@Shakils-MacBook-Air.local>
2025-04-08 21:08:10 +0600

    done

---
+++ Week_1/day-3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<"HI how are you ggggggggg"<<endl;
+
+    cout<<x<<" hi......"<<endl;
+    
+
+    return 0;
+}
