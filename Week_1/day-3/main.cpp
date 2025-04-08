eb485b98f33b3d2aecf4dcf2202772957b69ea55
Shakil Khan <shakil10945@gmail.com>
2025-04-09 04:37:37 +0600

    done

--- Week_1/day-3/main.cpp
+++ Week_1/day-3/main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
     int x;
-    //cin>>x;
+    cin>>x;
     cout<<"HI how are you ggggggggg"<<endl;
 
     cout<<" hi......iiiiii"<<endl;
     cout<<"oooooooo"<<endl;
+
+    cout<<x<<" heere is x : "<<x<<endl;
 
     return 0;
 }
