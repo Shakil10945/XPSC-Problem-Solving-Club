6524ba1f9cb9cf0d742e0d5036541e77cbf259c9
Shakil Khan <shakil10945@gmail.com>
2025-04-11 19:34:54 +0600

    good

---
+++ Allumni_Community/codeforces_div_4_problem_2/B_Good_Kid.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> vec;
+
+void input()
+{
+    cin >> n;
+    vec.resize(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+    
+}
+
+void solve()
+{
+    input();
+
+    sort(vec.begin(),vec.end());
+    vec[0]++;
+    
+    cout<<accumulate(vec.begin(), vec.end(), 1LL, multiplies<>())<<endl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
