1b3591090d7cb0095364f4f3085c8a7137b0ba80
Shakil Khan <shakil10945@gmail.com>
2025-04-12 16:05:03 +0600

    end of 2

---
+++ Allumni_Community/codeforces_div_4_problem_2/B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        string str;
+        cin >> str;
+
+        int siz = 0;
+        for(int i=0; i<a; i++)
+        {
+            siz = max(str[i] - 'a' +1, siz);
+        }
+        cout<<siz<<endl;
+    }
+
+    return 0;
+}
