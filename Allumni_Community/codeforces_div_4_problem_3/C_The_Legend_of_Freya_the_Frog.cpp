d65c20f748ae61f0b369b40ce773292093dde32c
Shakil Khan <shakil10945@gmail.com>
2025-04-14 12:59:32 +0600

    good

--- Allumni_Community/codeforces_div_4_problem_3/C_The_Legend_of_Freya_the_Frog.cpp
+++ Allumni_Community/codeforces_div_4_problem_3/C_The_Legend_of_Freya_the_Frog.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--)
-    {
-        int x,y,k;
-        cin>>x>>y>>k;
-
-        cout<<((x>y)? 2*(x/k) + ((x%k)? 1: 0): 2*(y/k) + ((y%k)? 2: 0))<<endl;
-
-
+    while (t--) {
+        long long x, y, k;
+        cin >> x >> y >> k;
+        cout << max(2* ((x+k-1)/k)-1, 2*((y+k-1)/k)) << '\n';
     }
 
     return 0;
 }
-
