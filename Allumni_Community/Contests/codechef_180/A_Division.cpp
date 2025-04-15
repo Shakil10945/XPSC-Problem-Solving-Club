c2850a986c115c651928a83a6cb99940401a932a
Shakil Khan <shakil10945@gmail.com>
2025-04-15 22:38:59 +0600

    gittss

---
+++ Allumni_Community/Contests/codechef_180/A_Division.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n< 1400)
+        {
+            cout<<"Division 4"<<endl;
+            continue;
+        }
+        else if(n< 1600)
+        {
+            cout<<"Division 3"<<endl;
+            continue;
+        }
+        else if(n< 1900)
+        {
+            cout<<"Division 2"<<endl;
+            continue;
+        }
+        else
+        {
+            cout<<"Division 1"<<endl;
+            continue;
+        }
+    }
+
+    return 0;
+}
+
