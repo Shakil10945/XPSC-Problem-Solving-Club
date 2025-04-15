83962ddc22b40569e858772957078630ddfcb4ea
Shakil Khan <shakil10945@gmail.com>
2025-04-15 19:50:17 +0600

    rr

--- Allumni_Community/Contests/codechef_180/Shall_we_play_a_game.cpp
+++ Allumni_Community/Contests/codechef_180/Shall_we_play_a_game.cpp
@@ -1,21 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-
-        
-        
+int main() {
+	// your code goes here
+    long long mod=998244353;
+    long long  t;cin>>t;
+    while(t--){
+        long long n;cin>>n;
+        long long ans=0;
+        long long maxi=0,mini=0;
+        long long d=1;
+        if(n%2){
+            long long x=1;
+            for(long long i=1;i<=n-1;i++){
+                x=x%mod;
+                x=x*2;
+            }
+            x=x%mod;
+            cout<<x<<endl;
+        }
+        else{
+            long long x1=1,x2=1;
+            for(long long i=1;i<=n-2;i++){
+                x1=x1%mod;
+                x1=x1*2;
+            }
+            x1=x1%mod;
+            long long x=1;
+            for(long long i=1;i<=n;i++){
+                x2=x2%mod;
+                x2=x2*2;
+            }
+            x2=x2%mod;
+            cout<<(x2-x1+mod)%mod<<endl;
+        }
     }
-
-    return 0;
 }
-
