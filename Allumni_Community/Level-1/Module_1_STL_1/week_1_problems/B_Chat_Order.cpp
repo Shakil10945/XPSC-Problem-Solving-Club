77da12ceb956430f2da4f5f339d44cd040b0de34
Shakil Khan <shakil10945@gmail.com>
2025-04-17 23:46:22 +0600

    ff

---
+++ Allumni_Community/Level-1/Module_1_STL_1/week_1_problems/B_Chat_Order.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    vector<string>vec;
+    while (t--)
+    {
+        string str;
+        cin>>str;
+
+        vec.push_back(str);
+    }
+
+    set<string>myset;
+
+    for(int i=t-1; i>=0; i--)
+    {
+        if(myset.count(vec[i]))
+            continue;
+        cout<<vec[i]<<endl;
+
+        myset.insert(vec[i]);
+    }
+
+    cout<<33<<endl;
+
+    return 0;
+}
+
