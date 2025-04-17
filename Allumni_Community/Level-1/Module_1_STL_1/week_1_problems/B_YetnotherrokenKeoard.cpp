33f1144c8f6b8ec1a6d14bb87a5ad47c070c11c6
Shakil Khan <shakil10945@gmail.com>
2025-04-17 15:46:31 +0600

    yetamari

--- Allumni_Community/Level-1/Module_1_STL_1/week_1_problems/B_YetnotherrokenKeoard.cpp
+++ Allumni_Community/Level-1/Module_1_STL_1/week_1_problems/B_YetnotherrokenKeoard.cpp
@@ -12,8 +12,27 @@ int main()
     {
         string str;
         cin>>str;
+        vector<int> vec;
+        vector<int> upp, loww;
+        for(int i=0;i<str.size(); i++)
+        {
+            if(isupper(str[i]) && str[i] != 'B') upp.push_back(i);
+            if(islower(str[i]) && str[i] != 'b') loww.push_back(i);
 
-        
+            if(str[i] == 'B' && !upp.empty())   upp.pop_back();
+            if(str[i] == 'b' && !loww.empty())   loww.pop_back();
+        }
+        vec.insert(vec.end(),upp.begin(),upp.end());
+        vec.insert(vec.end(),loww.begin(),loww.end());
+
+        sort(vec.begin(),vec.end());
+        //cout<<vec.size()<<endl;
+
+        for(int i=0; i<vec.size(); i++)
+        {
+            cout<<str[vec[i]];
+        }
+        cout<<endl;
     }
 
     return 0;
