188a280a384087925cd34ebd886e1412fec178df
Shakil Khan <shakil10945@gmail.com>
2025-04-19 00:36:11 +0600

    prac

---
+++ Allumni_Community/Level-1/M_2_STL_2/week_2_problems/Chemistry.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b; cin>>a>>b;
+
+        string str; cin>>str;
+
+        map<char, int> my_map;
+        for(auto x: str)
+            my_map[x]++;
+
+        int odd = 0;
+        for(auto x: my_map)
+        {
+            if(x.second%2!=0)
+                odd++;
+        }
+        cout<<(odd-1 > b? "NO" : "YES")<<endl;
+
+    }
+
+    return 0;
+}
+
