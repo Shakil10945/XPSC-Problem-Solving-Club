93421ddfe77866e17fa713934084ce73a9ebf4b1
Shakil Khan <shakil10945@gmail.com>
2025-04-20 12:06:40 +0600

    nw sY

--- Allumni_Community/Level-1/M_2_STL_2/week_2_problems/B_2_The_Strict_Teacher_Hard_Version.cpp
+++ Allumni_Community/Level-1/M_2_STL_2/week_2_problems/B_2_The_Strict_Teacher_Hard_Version.cpp
@@ -1,18 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        
-    }
-
-    return 0;
-}
-
+  
