86ffdd20bbb257eb2ba5b83a992d1ca2eb094811
Shakil Khan <shakil10945@gmail.com>
2025-04-23 22:23:23 +0600

    com

--- Allumni_Community/Level-1/Module_4_Two_pointers_technique_variable_size_sliding_window/Week_4_problems/S_to_T.cpp
+++ Allumni_Community/Level-1/Module_4_Two_pointers_technique_variable_size_sliding_window/Week_4_problems/S_to_T.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int n;
-string s,t;
+string s, t;
 
 void input()
 {
     cin >> n;
-    cin>>s>>t;
-    
+    cin >> s >> t;
 }
 
 void solve()
@@ -25,16 +24,56 @@ void solve()
 
     int s_one = it - s.begin();
     int t_one = itt - t.begin();
-    
+
     if(s_one != t_one)
     {
         cout<<-1<<endl;
         return;
     }
 
-    
-    
-    
+    set<int>myset;
+    for(int i=0; i<n; i++)
+    {
+        if(s[i] != t[i])
+        {
+            myset.insert(i);
+        }
+    }
+    int treverse = 0;
+    vector<int> ans;
+    for(int i=n-1; i>=0; i--)
+    {
+        if(s[i] =='1')
+        {
+            if(!myset.empty() && *myset.rbegin()>i)
+            {
+                int temp_i = i;
+                int need_set = *myset.rbegin() - i;
+                int x = *myset.rbegin();
+                while (need_set--)
+                {
+                    s[temp_i] = '1';
+                    if(s[temp_i]!= t[temp_i])
+                    {
+                        myset.insert(temp_i);
+                    }
+                    ans.push_back(temp_i+1);
+                    temp_i++;
+                }
+                if(x!= *myset.rbegin())
+                    i=temp_i;
+                
+                while (!myset.empty() && *myset.rbegin()>i)
+                {
+                    myset.erase(--myset.end());
+                }
+            }
+        }
+    }
+    cout<<ans.size()<<endl;
+    for(auto x: ans)
+        cout<<x<<" ";
+    cout<<endl;
 }
 
 int main()
