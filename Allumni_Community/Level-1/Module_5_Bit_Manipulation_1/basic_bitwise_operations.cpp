027a1e2f0b4c96c3a8d35dda5f61d15dad7eae9a
Shakil Khan <shakil10945@gmail.com>
2025-04-26 11:53:33 +0600

    bit operation

---
+++ Allumni_Community/Level-1/Module_5_Bit_Manipulation_1/basic_bitwise_operations.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int check_kth_bit_on_or_off(int n, int k)
+{
+    return (n>>k)&1;
+}
+
+void print_on_and_off_bits(int n)
+{
+    for(int k=30; k>=0; k--)
+    {
+        if(check_kth_bit_on_or_off(n,k))
+            cout<<1<<" ";
+        else
+            cout<<0<<" ";
+    }
+}
+
+int turn_on_kth_bit(int n, int k)
+{
+    return(n| (1<<k));
+}
+
+int toggle_kth_bit(int n, int k)
+{
+    return (n ^ (1<<k));
+}
+
+
+
+
+
+int main()
+{
+    ios:: sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 45;
+    cout<<check_kth_bit_on_or_off(n,4)<<endl;
+
+    print_on_and_off_bits(n);
+
+    cout<<turn_on_kth_bit(n, 3)<<endl;
+
+    cout<<toggle_kth_bit(n, 5)<<endl;
+
+    return 0;
+
+
+}
