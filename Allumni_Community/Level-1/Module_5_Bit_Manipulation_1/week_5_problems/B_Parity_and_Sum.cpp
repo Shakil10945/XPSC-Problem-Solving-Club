636ebaa4265bcbc96131b6d4f5048c3ef3fcfd2e
Shakil Khan <shakil10945@gmail.com>
2025-04-26 15:13:44 +0600

    new day

---
+++ Allumni_Community/Level-1/Module_5_Bit_Manipulation_1/week_5_problems/B_Parity_and_Sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> vec;
+
+void input()
+{
+    cin >> n;
+    vec.resize(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+    
+}
+
+void solve()
+{
+    input();
+    
+    set<int> myset;
+    for(auto x: vec)
+        myset.insert(x%2);
+    if(myset.size()==1)
+    {
+        cout<<0<<endl;  return;
+    }
+
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
