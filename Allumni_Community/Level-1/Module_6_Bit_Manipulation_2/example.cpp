86b3d2ece379ee85094101192c217b0071978328
Shakil Khan <shakil10945@gmail.com>
2025-04-29 22:30:07 +0600

    29

---
+++ Allumni_Community/Level-1/Module_6_Bit_Manipulation_2/example.cpp
@@ -0,0 +1,13 @@
+int ans =0;
+    for(int k=B; k>=0; k--)
+    {
+        if(bits[k]==n)
+        {
+            ans+=(1LL<<k);
+        }
+        else
+        {
+            int need = n- 
+        }
+
+    }
