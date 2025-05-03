283a5a5f853c6e4f34858be11f98866cb216f0ec
Shakil Khan <shakil10945@gmail.com>
2025-05-03 12:59:24 +0600

    11

---
+++ Allumni_Community/Level-1/Module_6_Bit_Manipulation_2/week_7_problems/A_Min_Or_Sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> vec;
+
+void input()
+{
+    cin >> n;
+    vec.resize(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+    
+}
+
+void solve()
+{
+    input();
+    
+    int ans = 0;
+    for(auto x: vec)
+        ans |=x;
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
