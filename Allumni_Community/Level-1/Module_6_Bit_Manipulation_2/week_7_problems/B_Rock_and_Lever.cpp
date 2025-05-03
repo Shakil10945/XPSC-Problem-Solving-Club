d9ba10bde1da2d6285c9f3afa248aafd15e492e7
Shakil Khan <shakil10945@gmail.com>
2025-05-03 19:10:37 +0600

    13

--- Allumni_Community/Level-1/Module_6_Bit_Manipulation_2/week_7_problems/B_Rock_and_Lever.cpp
+++ Allumni_Community/Level-1/Module_6_Bit_Manipulation_2/week_7_problems/B_Rock_and_Lever.cpp
@@ -17,6 +17,36 @@ void input()
 void solve()
 {
     input();
+    map<int, int> mp;
+    for(auto x: vec)
+    {
+        int b = (int)log2(x);
+        mp[b]++;
+    }
+
+    long long ans =0;
+    sort(vec.begin(),vec.end());
+    for(int i=0; i<n; i++)
+    {
+        int x = (int)log2(vec[i]);
+
+        for(auto it = mp.begin(); it!=mp.end();)
+        {
+            if(x==it->first)
+            {
+                if(it->second >1)
+                    ans+=(it->second-1);
+                it->second--;
+                if(!it->second)
+                    it = mp.erase(it);
+                else
+                    it++;
+            }
+            else
+                it++;
+        }
+    }
+    cout<<ans<<endl;
     
     
 }
