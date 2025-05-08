8c5958c7d9a5909a97f11666bf46478bf589248b
Shakil Khan <shakil10945@gmail.com>
2025-05-08 11:32:58 +0600

    done

--- Allumni_Community/Level-1/Module_7_Number_Theory_1/week_8_problems/GCD_Summation.cpp
+++ Allumni_Community/Level-1/Module_7_Number_Theory_1/week_8_problems/GCD_Summation.cpp
@@ -1,39 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-
     int t;
     cin >> t;
-
     while (t--) {
         int n, k;
         cin >> n >> k;
 
-        if (k % (n - 1) != 0) {
-            cout << -1 << '\n';
-            continue;
+        if (k < n - 1) {
+            cout << -1 << endl;
+        } else {
+            k -= (n - 2);
+            vector<int> ans;
+            ans.push_back(k);
+            ans.push_back(2 * k);
+            for (int i = 2 * k + 1; i < 2 * k + n - 1; ++i) {
+                ans.push_back(i);
+            }
+
+            for (int i = 0; i < ans.size(); ++i) {
+                cout << ans[i] << " ";
+            }
+            cout << endl;
         }
-
-        int x = k / (n - 1);
-        vector<int> a;
-
-        // Make sure values don't exceed 1e9
-        if (1LL * x * 2 + (n - 2) > 1e9) {
-            cout << -1 << '\n';
-            continue;
-        }
-
-        a.push_back(x);
-        for (int i = 1; i < n - 1; ++i) {
-            a.push_back(x * 2 + i); // Ensure uniqueness
-        }
-        a.push_back(x * 2);
-
-        for (int val : a) cout << val << " ";
-        cout << '\n';
     }
-
     return 0;
 }
--- Allumni_Community/Level-1/Module_7_Number_Theory_1/week_8_problems/example.cpp
+++ Allumni_Community/Level-1/Module_7_Number_Theory_1/week_8_problems/example.cpp
@@ -1,49 +1,45 @@
-#include "bits/stdc++.h"
 
-#ifdef LOCAL
-#include "F:\CPP\Debug\debug.h" 
-#else
-#define print(...) 1;
-#endif
-
-using i64 = long long;
-
-void solve()
-{
-    int n;
-    std::cin >> n;
-
-    std::vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        std::cin >> a[i];
-
-    int ans = 1e9;
-    for (int i = 0; i < n; i++)  
-    {
-        int cur_ans = 0;
-        for (int j : {i - 1, i + 1})
-        {
-            if (j < 0 or j >= n)
-                continue;
-
-            cur_ans = std::max(cur_ans, std::abs(a[i] - a[j]));
-        }
-        ans = std::min(ans, cur_ans);
-    }
-
-    std::cout << ans << "\n";
-}
+#include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int t = 1;
-    std::cin >> t;
-    
+    int t;
+    cin >> t;
     while (t--)
-        solve();
-    
+    {
+        int n, k;
+        cin>>n>>k;
+
+        if(n>k+1)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        if(n==k+1)
+        {
+            int x = 1;
+            for(int i=1;i<=n;i++)
+            {
+                cout<<x<<" ";
+                x++;
+            }
+            continue;
+        }
+        int j=1;
+        cout<<(k-(n-2))<<" "<<2*(k-(n-2))<<" ";
+        for(int i=0;i<n-2;i++)
+        {
+            cout<<j<<" ";
+            j++;
+        }
+        cout<<endl;
+
+    }
+
     return 0;
 }
+
