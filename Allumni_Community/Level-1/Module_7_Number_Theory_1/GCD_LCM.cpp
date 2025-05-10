4599208d8052681ddee7b0700f5eeef842a85381
Shakil Khan <shakil10945@gmail.com>
2025-05-10 20:19:37 +0600

    week

---
+++ Allumni_Community/Level-1/Module_7_Number_Theory_1/GCD_LCM.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    return __gcd(a,b);  //O(log(min(a,b)))
+}
+
+int LCM(int a, int b)
+{
+    //return ((a*b)/ __gcd(a,b)); // O(log(min(a,b)));
+    return (a/__gcd(a,b))*b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b;    cin>>a>>b;
+    cout<<GCD(a,b)<<endl;
+    cout<<LCM(a,b)<<endl;
+
+    return 0;
+}
