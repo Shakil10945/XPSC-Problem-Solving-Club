367cf0a3b7f30a51d3a832caf8ab94dd71e8634e
Shakil Khan <shakil10945@gmail.com>
2025-05-14 01:19:22 +0600

    dd

---
+++ Allumni_Community/Level-1/Module_7_Number_Theory_1/DS_Algo/A_Dinner_Time.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#include<bits/stdc++.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    //f
+    char a[100];
+    getchar();
+    fgets(a,sizeof(a),stdin);
+    
+    printf("%d\n",n);
+    printf("%s ",a);
+    
+    
+
+    return 0;
+}
