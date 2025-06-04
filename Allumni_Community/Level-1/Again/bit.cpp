af4f72fbb4b13a86619608bc52d339d16adfc475
Shakil Khan <shakil10945@gmail.com>
2025-06-04 23:08:18 +0600

    dd

---
+++ Allumni_Community/Level-1/Again/bit.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int check_kth_bit_on_or_off(int n, int k)
+{
+    return (n>>k)&1;
+}
+void print_on_and_off_bits(int n)
+{
+    for(int k=0; k>=0; k--)
+    {
+        if(check_kth_bit_on_or_off(n,k))
+            cout<<1<<" ";
+        else
+            cout<<0<<" ";
+    }
+}
+int turn_on_kth_bit(int n, int k)
+{
+    return(n|(1<<k));
+}
+int turn_of_kth_bit(int n, k)
+{
+    return (n&( ~(1<<k)));
+}
+
+int toggle_kth_bit(int n, int k)
+{
+    return(n^(1<<k));
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n= 45;
+    cout<<
+
+    return 0;
+}
+
