070d1d1c00c0a9d97e6c8a0dc41b10507b400c4b
Shakil Khan <shakil10945@gmail.com>
2025-06-25 19:02:28 +0600

    pp

---
+++ Allumni_Community/Level-1/Module_9_Binary_Search-2/problem_set/C_Save_More_Mice.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+vector<int> vec;
+
+void input()
+{
+    cin >> n >> k;
+    vec.resize(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+    
+}
+
+void solve()
+{
+    input();
+
+
+    
+    
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
